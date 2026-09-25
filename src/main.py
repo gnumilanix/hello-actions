0f60a008afa4e13a6bfaf62492d4cfb8840c1d9f
milan.rajbhandari <milan.rajbhandari@okg.com>
2026-09-25 22:08:10 +0800

    add: application to support non debug mode

--- src/main.py
+++ src/main.py
@@ -3,4 +3,5 @@ from greetings import app
 
 if __name__ == '__main__':
     host_env = os.environ.get('FLASK_RUN_HOST', '127.0.0.1')
-    app.run(host=host_env, port=8080, debug=True)
+    is_debug = os.environ.get('FLASK_DEBUG', 'true').lower() == 'true'
+    app.run(host=host_env, port=8080, debug=is_debug)
