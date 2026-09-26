ad7b622602c2faaa54983c16f616e2e35a17a2f2
milan.rajbhandari <milan.rajbhandari@okg.com>
2026-09-26 10:19:17 +0800

    upt: enabled some tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,15 +1,15 @@
-# import runpy
-# from unittest.mock import patch
+import runpy
+from unittest.mock import patch
 
 
-# def test_main_starts_app_with_environment_configuration(monkeypatch):
-#     monkeypatch.setenv('FLASK_RUN_HOST', '0.0.0.0')
-#     monkeypatch.setenv('FLASK_DEBUG', 'false')
+def test_main_starts_app_with_environment_configuration(monkeypatch):
+    monkeypatch.setenv('FLASK_RUN_HOST', '0.0.0.0')
+    monkeypatch.setenv('FLASK_DEBUG', 'false')
 
-#     with patch('flask.Flask.run') as run:
-#         runpy.run_path('src/main.py', run_name='__main__')
+    with patch('flask.Flask.run') as run:
+        runpy.run_path('src/main.py', run_name='__main__')
 
-#     run.assert_called_once_with(host='0.0.0.0', port=8080, debug=False)
+    run.assert_called_once_with(host='0.0.0.0', port=8080, debug=False)
 
 
 # def test_main_uses_default_configuration(monkeypatch):
