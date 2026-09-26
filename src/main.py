37f82874b8d0b51961c893a1cda9ccdcb1a78b02
milan.rajbhandari <milan.rajbhandari@okg.com>
2026-09-26 10:25:57 +0800

    upt: enabled tests
    style: updated formatting of py files

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 import os
 from greetings import app
 
-if __name__ == '__main__':
-    host_env = os.environ.get('FLASK_RUN_HOST', '127.0.0.1')
-    is_debug = os.environ.get('FLASK_DEBUG', 'true').lower() == 'true'
+if __name__ == "__main__":
+    host_env = os.environ.get("FLASK_RUN_HOST", "127.0.0.1")
+    is_debug = os.environ.get("FLASK_DEBUG", "true").lower() == "true"
     app.run(host=host_env, port=8080, debug=is_debug)
--- tests/test_main.py
+++ tests/test_main.py
@@ -3,20 +3,20 @@ from unittest.mock import patch
 
 
 def test_main_starts_app_with_environment_configuration(monkeypatch):
-    monkeypatch.setenv('FLASK_RUN_HOST', '0.0.0.0')
-    monkeypatch.setenv('FLASK_DEBUG', 'false')
+    monkeypatch.setenv("FLASK_RUN_HOST", "0.0.0.0")
+    monkeypatch.setenv("FLASK_DEBUG", "false")
 
-    with patch('flask.Flask.run') as run:
-        runpy.run_path('src/main.py', run_name='__main__')
+    with patch("flask.Flask.run") as run:
+        runpy.run_path("src/main.py", run_name="__main__")
 
-    run.assert_called_once_with(host='0.0.0.0', port=8080, debug=False)
+    run.assert_called_once_with(host="0.0.0.0", port=8080, debug=False)
 
 
-# def test_main_uses_default_configuration(monkeypatch):
-#     monkeypatch.delenv('FLASK_RUN_HOST', raising=False)
-#     monkeypatch.delenv('FLASK_DEBUG', raising=False)
+def test_main_uses_default_configuration(monkeypatch):
+    monkeypatch.delenv("FLASK_RUN_HOST", raising=False)
+    monkeypatch.delenv("FLASK_DEBUG", raising=False)
 
-#     with patch('flask.Flask.run') as run:
-#         runpy.run_path('src/main.py', run_name='__main__')
+    with patch("flask.Flask.run") as run:
+        runpy.run_path("src/main.py", run_name="__main__")
 
-#     run.assert_called_once_with(host='127.0.0.1', port=8080, debug=True)
+    run.assert_called_once_with(host="127.0.0.1", port=8080, debug=True)
