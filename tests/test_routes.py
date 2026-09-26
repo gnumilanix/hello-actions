f6eab4f8006825b325cb73da8c39c5eb51b1adcc
milan.rajbhandari <milan.rajbhandari@okg.com>
2026-09-26 10:25:57 +0800

    upt: enabled tests
    style: updated formatting of py files

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -4,15 +4,15 @@ from greetings import app
 def test_hello_world_returns_message():
     client = app.test_client()
 
-    response = client.get('/')
+    response = client.get("/")
 
     assert response.status_code == 200
-    assert response.get_json() == {'message': 'Hello World'}
+    assert response.get_json() == {"message": "Hello World"}
 
 
 def test_hello_world_rejects_post():
     client = app.test_client()
 
-    response = client.post('/')
+    response = client.post("/")
 
     assert response.status_code == 405
